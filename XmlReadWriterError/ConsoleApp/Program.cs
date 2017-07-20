46e0cdb8595e03df3a61562506154d9bba167bf2
Mauricio Benavides <MauricioB@Qvella.com>
2017-07-20 15:13:17 -0400

    This application reproduces a failure I am trying to troubleshoot.

---
+++ XmlReadWriterError/ConsoleApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XmlDotNetSandardClass;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DotNetStandardClass failingClass = new DotNetStandardClass();
+            try
+            {
+                failingClass.MethodThatFails();
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message + Environment.NewLine + ex.StackTrace + Environment.NewLine + ex.InnerException);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ XmlReadWriterError/XmlDotNetSandardClass/DotNetStandardClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace XmlDotNetSandardClass
+{
+    public class DotNetStandardClass
+    {
+        public void MethodThatFails()
+        {
+
+            XDocument doc = new XDocument(
+               new XComment("This is a comment"),
+               new XElement("Root",
+                   new XElement("Child1", "data1"),
+                   new XElement("Child2", "data2"),
+                   new XElement("Child3", "data3")
+               )
+           );
+
+            foreach (XNode n in doc.Nodes())
+            {
+                if (n.NodeType == XmlNodeType.Element)
+                {
+                    Debug.WriteLine(n.ToString());
+                    break;
+                }
+
+            }
+        }
+    }
+}
